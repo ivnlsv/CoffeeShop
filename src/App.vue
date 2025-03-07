fab8d635fe764b2d671aad3f48146accacc0a7b4
ivnlsv <ivan.losev@denuo.legal>
2025-03-07 12:10:05 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div id="app">
-    Hello Vue
+  <div class="wrapper">
+   <hero-view />
+   <footer-component /> 
   </div>
 </template>
 
 <script>
 
+import HeroView from './views/HeroView.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
 export default {
-  
+  components: {HeroView, FooterComponent},
 }
 </script>
 
