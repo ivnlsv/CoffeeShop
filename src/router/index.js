82507526c441b81b675c7efba5a26319a5f80659
ivnlsv <ivan.losev@denuo.legal>
2025-03-11 12:42:47 +0300

    upd

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HeroView from '../views/HeroView.vue'
 import OurCoffeeView from '../views/OurCoffeeView.vue'
 import GoodsView from '../views/GoodsView.vue'
 import ContactsView from '@/views/ContactsView.vue'
+import ThanksView from '@/views/ThanksView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,7 +12,8 @@ const routes = [
     { path: '/', component: HeroView },
     { path: '/our-coffee', component: OurCoffeeView },
     { path: '/goods', component: GoodsView },
-    {path: '/contacts', component: ContactsView}
+    { path: '/contacts', component: ContactsView },
+    {path: '/thank-you', component: ThanksView}
 ]
 
 const router = new VueRouter({
