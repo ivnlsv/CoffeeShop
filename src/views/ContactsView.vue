4a669159d1db3d6f588e006aeccc00fbd07d6787
ivnlsv <ivan.losev@denuo.legal>
2025-03-20 16:29:58 +0300

    upd

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -23,40 +23,64 @@
 
 						<form @submit.prevent="submit($event)" action="/" class="mt-5">
 							<div class="form-group row">
-								<div class="col col-12 col-sm-3 d-flex align-items-center">
+								<div class="col col-12 col-sm-3 d-flex align-items-start">
 									<label for="name-input" class="mb-0">
 										Name
 										<span style="color: red">*</span>
 									</label>
 								</div>
 								<div class="col col-12 col-sm-9">
-									<input v-model="form.text" type="text" class="form-control" id="name-input" />
+									<input
+										v-model="v$.name.$model"
+										type="text"
+										class="form-control"
+										id="name-input"
+									/>
+									<span v-for="error in v$.name.$errors" :key="error.$uid">
+										{{ error.$message }}
+									</span>
 								</div>
 							</div>
 
 							<div class="form-group row">
-								<div class="col col-12 col-sm-3 d-flex align-items-center">
+								<div class="col col-12 col-sm-3 d-flex align-items-start">
 									<label for="email-input" class="mb-0">
 										E-mail
 										<span style="color: red">*</span>
 									</label>
 								</div>
 								<div class="col col-12 col-sm-9">
-									<input v-model="form.email" type="email" class="form-control" id="email-input" />
+									<input
+										v-model="v$.email.$model"
+										type="email"
+										class="form-control"
+										id="email-input"
+									/>
+									<span v-for="error in v$.email.$errors" :key="error.$uid">
+										{{ error.$message }}
+									</span>
 								</div>
 							</div>
 
 							<div class="form-group row">
-								<div class="col col-12 col-sm-3 d-flex align-items-center">
+								<div class="col col-12 col-sm-3 d-flex align-items-start">
 									<label for="phone-input" class="mb-0"> Phone </label>
 								</div>
 								<div class="col col-12 col-sm-9">
-									<input v-model="form.tel" type="tel" class="form-control" id="phone-input" />
+									<input
+										v-model="v$.phone.$model"
+										type="tel"
+										class="form-control"
+										id="phone-input"
+									/>
+									<span v-for="error in v$.phone.$errors" :key="error.$uid">
+										{{ error.$message }}
+									</span>
 								</div>
 							</div>
 
 							<div class="form-group row textarea">
-								<div class="col col-12 d-flex justify-content-center">
+								<div class="col col-12 d-flex justify-content-start">
 									<label for="pmessage" class="mb-3 mt-3 text-center">
 										Your message
 										<span style="color: red">*</span>
@@ -64,14 +88,29 @@
 								</div>
 								<div class="col col-12">
 									<textarea
-									    v-model="form.message"
+										v-model="v$.message.$model"
 										class="form-control"
 										name="message"
 										id="message"
 										rows="5"
 										placeholder="Leave your comments here"
 									></textarea>
+									<span v-for="error in v$.message.$errors" :key="error.$uid">
+										{{ error.$message }}
+									</span>
 								</div>
+							</div>
+							<div class="checkbox-box">
+								<div class="checkbox-items">
+									<input
+									v-model="isChecked"
+									type="checkbox"
+									name="agreed"
+									id="agreed"
+								/>
+								<label for="agreed">Согласен с договором оферты</label>
+								</div>
+								<span v-if="!isChecked" style="color: red;">Необходимо согласиться с договором оферты</span>
 							</div>
 
 							<div class="row">
@@ -89,25 +128,50 @@
 <script>
 import HeaderTitle from '@/components/HeaderTitle.vue';
 import NavBarComponent from '@/components/NavBarComponent.vue';
+import { useVuelidate } from '@vuelidate/core';
+import { required, email, maxLength } from '@vuelidate/validators';
+import { helpers } from '@vuelidate/validators';
+import { minLength } from '@/validators/minLength';
 
 export default {
 	components: { NavBarComponent, HeaderTitle },
+	setup() {
+		return { v$: useVuelidate() };
+	},
 	data() {
 		return {
 			text: 'Contact us',
-			form: {
-				text: '',
-				email: '',
-				tel: '',
-				message: ''
-			}
+			name: '',
+			email: '',
+			phone: '',
+			message: '',
+			isChecked: true,
 		};
 	},
+	validations() {
+		return {
+			name: { required },
+			email: { required, email },
+			phone: {},
+			message: {
+				required,
+				maxLength: maxLength(20),
+				minLength: helpers.withMessage('this value less 5', minLength),
+			},
+			isChecked: {
+				required: true,
+			},
+		};
+	},
+
 	methods: {
-    submit(event)
-    {
-      console.log(event.target)
-     },
-  }
+		async submit() {
+			const isFormCorrect = await this.v$.$validate();
+
+			if (!isFormCorrect || !this.isChecked) {
+				console.log('Ошибка отправки', this.isChecked);
+			}
+		},
+	},
 };
 </script>
