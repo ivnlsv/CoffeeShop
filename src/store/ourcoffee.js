9fe33e4914563bc48619438ad911f93ca7bdecf1
ivnlsv <ivan.losev@denuo.legal>
2025-03-24 12:27:43 +0300

    upd

--- src/store/ourcoffee.js
+++ src/store/ourcoffee.js
@@ -1,43 +1,6 @@
 const ourCoffee = {
 	state: {
-		ourcoffee: [
-			{
-				id: 0,
-				title: 'Solimo Coffee Beans 2kg',
-				price: 10.73,
-				image: 'coffee-1.jpg',
-			},
-			{
-				id: 1,
-				title: 'Presto Coffee Beans 1kg',
-				price: 15.99,
-				image: 'coffee-2.jpg',
-			},
-			{
-				id: 2,
-				title: ' AROMISTICO Coffee 1kg',
-				price: 6.99,
-				image: 'coffee-3.jpg',
-			},
-			{
-				id: 3,
-				title: 'Solimo Coffee Beans 2kg',
-				price: 10.73,
-				image: 'coffee-3.jpg',
-			},
-			{
-				id: 4,
-				title: 'Solimo Coffee Beans 2kg',
-				price: 10.73,
-				image: 'coffee-3.jpg',
-			},
-			{
-				id: 5,
-				title: ' AROMISTICO Coffee 1kg',
-				price: 10.73,
-				image: 'coffee-3.jpg',
-			},
-		],
+		ourcoffee: [],
 	},
 	mutations: {
 		setCoffeeData(state, data) {
